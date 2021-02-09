c3d6c76eba154c302ac1acacc70f5de688acfdbe
Rendi K <rendi.krinaldi@gmail.com>
2021-02-09 11:59:29 +0700

    create food table

---
+++ database/migrations/2021_02_09_045416_create_food_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFoodTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        if (!Schema::hasTable('foods')) {
+            Schema::create('foods', function (Blueprint $table) {
+                $table->id();
+
+                $table->string('name');
+                $table->text('description')->nullable();
+                $table->text('ingredients')->nullable();
+                $table->double('price')->default(0);
+                $table->double('rate')->nullable();
+                $table->string('types')->nullable();
+                $table->text('picture_path')->nullable();
+
+                $table->softDeletes();
+                $table->timestamps();
+            });
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('foods');
+    }
+}
