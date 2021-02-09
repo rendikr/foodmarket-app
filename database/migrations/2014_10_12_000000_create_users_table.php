0b0a068ab70a44f4c5fbd589fb4f8d9a9f281921
Rendi K <rendi.krinaldi@gmail.com>
2021-02-09 11:55:49 +0700

    create database user

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,17 +13,28 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->foreignId('current_team_id')->nullable();
-            $table->text('profile_photo_path')->nullable();
-            $table->timestamps();
-        });
+        if (!Schema::hasTable('foods')) {
+            Schema::create('users', function (Blueprint $table) {
+                $table->id();
+                $table->string('name');
+                $table->string('email')->unique();
+                $table->timestamp('email_verified_at')->nullable();
+                $table->string('password');
+                $table->rememberToken();
+
+                $table->foreignId('current_team_id')->nullable();
+                $table->text('profile_photo_path')->nullable();
+
+                $table->text('address')->nullable();
+                $table->string('house_number')->nullable();
+                $table->string('phone_number')->nullable();
+                $table->string('city')->nullable();
+
+                $table->string('roles')->default('USER');
+
+                $table->timestamps();
+            });
+        }
     }
 
     /**
