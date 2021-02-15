ea741f887a0c54dd64376d6dfb4200c0ab8712ea
Rendi K <rendi.krinaldi@gmail.com>
2021-02-15 18:53:01 +0700

    midtrans configuration

--- config/services.php
+++ config/services.php
@@ -12,11 +12,11 @@ return [
     | location for this type of information, allowing packages to have
     | a conventional file to locate the various service credentials.
     |
-    */
+     */
 
-    'mailgun' => [
-        'domain' => env('MAILGUN_DOMAIN'),
-        'secret' => env('MAILGUN_SECRET'),
+    'mailgun'  => [
+        'domain'   => env('MAILGUN_DOMAIN'),
+        'secret'   => env('MAILGUN_SECRET'),
         'endpoint' => env('MAILGUN_ENDPOINT', 'api.mailgun.net'),
     ],
 
@@ -24,10 +24,18 @@ return [
         'token' => env('POSTMARK_TOKEN'),
     ],
 
-    'ses' => [
-        'key' => env('AWS_ACCESS_KEY_ID'),
+    'ses'      => [
+        'key'    => env('AWS_ACCESS_KEY_ID'),
         'secret' => env('AWS_SECRET_ACCESS_KEY'),
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
+    ],
+
+    'midtrans' => [
+        'serverKey'    => env('MIDTRANS_SERVER_KEY'),
+        'clientKey'    => env('MIDTRANS_CLIENT_KEY'),
+        'isProduction' => env('MIDTRANS_IS_PRODUCTION', false),
+        'isSanitized'  => env('MIDTRANS_IS_SANITIZED', true),
+        'is3ds'        => env('MIDTRANS_IS_3DS', true),
     ],
 
 ];
